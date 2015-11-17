1eace8e4624b9c9e9d8302a9f0bfba30536fec57
kalvin <kalvin@helpio.com>
2015-11-17 22:10:25 +0800

    Add notification attributes

--- app/controllers/api/notification_controller.rb
+++ app/controllers/api/notification_controller.rb
@@ -5,8 +5,14 @@ class Api::NotificationController < ApplicationController
     @notification = Notification.order('id desc').where(uid: session[:uid])
 	@notification.each do |notification|
 	  user = User.find(notification.suid)
+	  pin = Pin.find(notification.pin_id)
 	  notification.nickname = user.nickname
 	  notification.prof_img = user.prof_img
+	  notification.lat = pin.lat
+	  notification.lon = pin.lon
+      notification.comment_count = pin.comment.count
+      notification.like_count = pin.pin_like.count
+      notification.liked = pin.pin_like.where(uid: session[:uid]).count
 	end
     respond_to do |format|
       format.json { render :json => @notification }
