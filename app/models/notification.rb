e06b1db20b88bf85816a2fa4a5cd7444ea90d16a
kalvin <kalvin@helpio.com>
2015-11-22 17:29:35 +0800

    Add img attr to notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,8 +1,8 @@
 class Notification < ActiveRecord::Base
-  	attr_accessor :nickname, :prof_img, :lat, :lon, :liked, :comment_count, :like_count
+  	attr_accessor :nickname, :prof_img, :img, :lat, :lon, :liked, :comment_count, :like_count
 
   	def attributes
-  		super.merge({'nickname' => self.nickname, 'prof_img' => self.prof_img, 
+  		super.merge({'nickname' => self.nickname, 'prof_img' => self.prof_img, 'img' => self.img,
   					 'lat' => self.lat, 'lon' => self.lon, 'liked' => self.liked, 
   					 'comment_count' => self.comment_count, 'like_count' => self.like_count})
 	end
