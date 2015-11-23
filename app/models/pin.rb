435e1f1507932d9f07406abc443b28f61b4235e4
paugoo <khandilit@gmail.com>
2015-11-23 13:38:48 +0800

    Add resize function for images

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,8 @@ class Pin < ActiveRecord::Base
   	has_many :comment, dependent: :destroy
   	has_many :pin_like, dependent: :destroy
   	attr_accessor :nickname, :prof_img, :comment_count, :like_count, :liked
+	require 'rubygems'
+	require 'ImageResize'
 
   	def attributes
   		super.merge({'nickname' => self.nickname, 'prof_img' => self.prof_img, 'comment_count' => self.comment_count, 'like_count' => self.like_count, 'liked' => self.liked})
@@ -10,9 +12,12 @@ class Pin < ActiveRecord::Base
 	def uploaded_file=(fileobj)
 	    name = unique_and_proper_filename(fileobj.original_filename)
 	    directory = "public/images/upload"
+	    resize_directory = "public/images/upload/resize"
 	    path = File.join(directory, name)
+	    resize_path = File.join(resize_directory, name)
 	    File.open(path, "wb") { |f| f.write(fileobj.read) }
 	    self.img = name
+	    Image.resize(path, resize_path, 50,50)
 	    return true
 	end
 
