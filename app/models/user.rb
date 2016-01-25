8d1f11393d02eb5b498f655e88f1e0b9e918aaea
kalvin <kalvin@helpio.com>
2016-01-25 13:20:51 +0800

    Use devise uid auto add

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-         # :recoverable, :rememberable, :trackable, :validatable, :omniauthable
-  # include DeviseTokenAuth::Concerns::User
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
   
 	has_many :comment, dependent: :destroy
 	has_many :pin, dependent: :destroy
